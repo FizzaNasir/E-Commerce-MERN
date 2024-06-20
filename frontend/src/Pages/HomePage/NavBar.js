8618ce45abc1e5cb28518be66b5676982054234f
FizzaNasir <fizzanasir976@gmail.com>
2024-06-20 16:11:56 +0500

    Added navbar

---
+++ frontend/src/Pages/HomePage/NavBar.js
@@ -0,0 +1,49 @@
+import { NavbarStyle } from "./Styles/NavBarStyle"
+import SearchIcon from '@mui/icons-material/Search';
+import { Search } from "./Styles/NavBarStyle";
+import { SearchIconWrapper } from "./Styles/NavBarStyle";
+import { StyledInputBase } from "./Styles/NavBarStyle";
+import { ParentStyle } from "./Styles/NavBarStyle";
+import { flexContainer } from "./Styles/NavBarStyle";
+import { Highlight } from "./Styles/NavBarStyle";
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+const NavBar = () => {
+   return(
+    <header style={ParentStyle}>
+       {/* <div style={highlights}> */}
+       <Highlight>
+            <p>Become a Seller</p>
+            <p>Daraz Affiliate Program </p>
+            <p>Help & Support</p>
+            <p> Daraz Logistics Partner</p>
+         {/* </div> */}
+         </Highlight>
+      <div style={NavbarStyle}> 
+        
+         {/* <div> */}
+            <img src="https://icms-image.slatic.net/images/ims-web/e650d6ca-1841-4646-b0e9-4ddbf2beb731.png" style={{width: "8%"}}></img>
+            <Search>
+               <SearchIconWrapper>
+               <SearchIcon />
+               </SearchIconWrapper>
+               <StyledInputBase
+               placeholder="Search"
+               inputProps={{ 'aria-label': 'search' }}
+               />
+            </Search>
+            <div style={flexContainer}>
+               <Person2OutlinedIcon/>
+               <p>Login</p>
+            </div>
+               <p>|</p>
+               <p>SignUp</p>
+               <p>EN</p>
+            <ShoppingCartOutlinedIcon/>
+          {/* </div> */}
+      </div>
+      </header>
+   )
+}
+export default NavBar
---
+++ frontend/src/Pages/HomePage/Styles/NavBarStyle.js
@@ -0,0 +1,89 @@
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+export const ParentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    color: "white",
+    backgroundColor: "rgb(245, 86, 32)",
+    fontSize: "12px",
+}
+
+export const NavbarStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    paddingBottom: "1%",
+
+}
+
+export const flexContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  
+}
+
+export const Highlight = styled('div')(({theme})=>(
+   {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginLeft: "10%",
+    '& > *': {   //will apply to all children
+        paddingRight: '15px',  
+      },
+      '& > *:hover': {
+        color: '#d7dbd7',
+        cursor: 'pointer'
+      },
+}))
+
+export const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    color: "grey",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 1),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 1),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+export const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: "grey"
+  }));
+
+ export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 80, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
+  
+ 
